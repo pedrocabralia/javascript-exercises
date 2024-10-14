03ce8b6e540dde20cb0dc2412e9ce7be73bed945
pedrocabralia <pedrocabralia@hotmail.com>
2024-10-14 10:05:34 -0300

    exercício 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+   
+     string = string.split("")
+     string = string.reverse()
+     string = string.join("")
 
+     return string
+     
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
+
+
