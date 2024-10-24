ecccd88a917e925ea93ad0dad3ce263d45493873
pedrocabralia <pedrocabralia@hotmail.com>
2024-10-24 11:10:58 -0300

    exercicio04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
 
-// Do not edit below this line
-module.exports = removeFromArray;
+const removeFromArray = function (array, ...args) {
+ 
+    const newArray = [];
+    
+    array.forEach((item) => {
+   
+      if (!args.includes(item)) {
+        newArray.push(item);
+      }
+    });
+  
+    return newArray;
+  };
+  
+
+console.table(removeFromArray(["banana","maça", "pera", "uva"],"uva", "maça"))
+  module.exports = removeFromArray;
+  
