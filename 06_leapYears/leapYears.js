6f2165a9185407a264b34acb40360716f6272fa3
pedrocabralia <pedrocabralia@hotmail.com>
2024-10-25 22:10:37 -0300

    exercicio 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,25 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
+
+
+    if(leapYear % 4 == 0 && leapYear %  10 !=0 ){
+        return true
+    }
+    else if(leapYear %  100 == 0 &&   leapYear %  400 != 0){
+
+        return false
+    }
+
+    else if(leapYear %  100 == 0 &&   leapYear %  400 == 0){
+
+        return true
+    }
+    else{
+        return false
+    }
 
 };
+
+
 
 // Do not edit below this line
 module.exports = leapYears;
