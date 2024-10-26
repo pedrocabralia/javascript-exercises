0a7059cecb30e4804fe1311a3d6183f20e8b1e7c
pedrocabralia <pedrocabralia@hotmail.com>
2024-10-25 22:24:13 -0300

    exercicio 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+
+    c = 5/9 * (f-32);
+
+    c = Math.round(c * 10) /10 ;
+    return c
+
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(c) {
 
+    f = (c * 1.8) + 32;
+    f = Math.round(f * 10)/10;
+
+    return f; 
+};
+console.log(convertToCelsius(100))
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
